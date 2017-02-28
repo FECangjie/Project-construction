e1f30b6d870703c1acb5d7f38ee7bac26a609b12
QinChao <qinchao@lianjia.com>
2017-02-28 15:44:56 +0800

    shortcut key

--- src/components/header0.js
+++ src/components/header0.js
@@ -9,24 +9,23 @@ export default props => {
   return (
     <div className="navbar navbar-default">
       <div className="navbar-header">
-        <a className="navbar-brand" href="#">你应该知道的Chrome开发者工具</a>
+        <a className="navbar-brand" href="#">非正常人类集中营</a>
       </div>
       <ul className="nav navbar-nav">
         <li className={classnames({'nav-item': true, 'active': props.path === '/'})}><Link to="/">首页</Link></li>
-        <li className={classnames({'nav-item': true, 'active': props.path === '/'})}><Link to="/">快捷键</Link></li>
         <li className={classnames({'nav-item': true, 'active': props.path === '/about'})}><Link to="/about">关于</Link></li>
       </ul>
       <form className="navbar-form navbar-left">
         <div className="input-group">
-          <input className="form-control" type="text" placeholder="并没有什么用的搜索框"/>
+          <input className="form-control" type="text" placeholder="输入搜索关键词"/>
           <div className="input-group-btn"><i className="iconfont icon-search"/></div>
         </div>
       </form>
       <div className="navbar-userinfo">
         <div className="navbar-panel">
-          <span className="info"><span className="name">神秘管理员</span></span>
-          <i className="divide"></i>
-          <span className="btn-login" data-mark="logout"></span>
+          <span className="info"><span className="name">哈喽</span> 你好</span>
+          <i className="divide">|</i>
+          <span className="btn-login" data-mark="logout">退出</span>
         </div>
       </div>
     </div>
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,110 @@
 // Learn more on how to config.
 // - https://github.com/ant-tool/atool-build#配置扩展
 
-module.exports = function(webpackConfig) {
-  webpackConfig.babel.plugins.push('transform-runtime');
-  webpackConfig.babel.plugins.push(['import', {
-    libraryName: 'antd',
-    style: 'css',
-  }]);
+// module.exports = function(webpackConfig) {
+//   webpackConfig.babel.plugins.push('transform-runtime');
+//   webpackConfig.babel.plugins.push(['import', {
+//     libraryName: 'antd',
+//     style: 'css',
+//   }]);
+//
+//   return webpackConfig;
+// };
 
-  return webpackConfig;
-};
+
+/**
+ * @file 开发环境配置
+ */
+
+var webpack = require('webpack')
+var CommonsChunkPlugin = webpack.optimize.CommonsChunkPlugin
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
+var path = require('path')
+var pageConf = require('./pageConf')
+
+var wpConfig = function () {
+  return {
+    entry: pageConf.getEntry('dev'),
+    watch: true,
+    debug: true,
+    devtool: 'source-map',
+    output: {
+      path: '/',
+      publicPath: '/asset/',
+      filename: '[name].js',
+      chunkFilename: '[name].chunk.js',
+    // chunkFilename: 'entry.biz.[hash].js'
+    },
+    module: {
+      preLoaders: [
+        {test: /\.json$/, loader: 'json'}
+      ],
+      loaders: [
+        {
+          test: /\.rt/,
+          loader: ['babel', 'react-templates?modules=amd']
+        },
+        {
+          test: /\.css$/,
+          loader:ExtractTextPlugin.extract("style-loader", "css-loader")
+        },
+        {
+          test: /\.jsx$/,
+          loader: ['babel'],
+          exclude: path.resolve(__dirname, '../node_modules')
+        },
+        {
+          test: /\.js$/,
+          loaders: ['babel'],
+          exclude: path.resolve(__dirname, '../node_modules')
+        },
+        {
+          test: /\.jsx$/,
+          include: paths.appSrc,
+          loader: 'babel',
+          query: {
+           plugins: [
+             ['import', [{ libraryName: "antd", style: 'css' }]],
+           ],
+            cacheDirectory: true
+          }
+        },
+        {
+          test: /\.(gif|jpg|png|woff|svg|eot|ttf)\??.*$/,
+          loader: 'url-loader?limit=50000&name=[name].[hash].[ext]'
+        }
+      // {test: /\.png$/, loader: 'url?mimetype=image/png'},
+      // {test: /\.jpg/, loader: 'url?mimetype=image/jpg'},
+      // {test: /\.jpeg/, loader: 'url?mimetype=image/jpeg'}
+      ]
+    },
+    loader: {
+      configEnvironment: 'dev'
+    },
+    resolve: {
+      extensions: ['', '.js', '.jsx', '.css', '.rt'],
+      alias: {
+        bizComponent: path.resolve(__dirname, '../src/client/bizComponent'),
+        common: path.resolve(__dirname, '../src/client/common'),
+        commonStyle: path.resolve(__dirname, '../src/client/commonStyle'),
+        component: path.resolve(__dirname, '../src/client/component'),
+        flagConf: path.resolve(__dirname, '../src/client/flagConf')
+      }
+    },
+    plugins: [
+      ["import", options],
+      new CommonsChunkPlugin({
+        name: 'common',
+        filename: 'common.js',
+        minChunks: Infinity
+      }),
+      new ExtractTextPlugin({
+        filename: '[name].css',
+        allChunks: true
+      }),
+      new webpack.HotModuleReplacementPlugin()
+    ]
+  }
+}
+
+module.exports = wpConfig
